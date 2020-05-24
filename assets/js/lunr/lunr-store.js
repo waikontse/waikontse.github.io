f5ffcd6056d0b52353986fcd868f491bf70b5fa3
Wai kon Tse <waikon@waikontse.info>
2020-05-24 18:42:09 +0200

    Update webpage

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "Flow - Worst methodology ever",
+        "excerpt":"A satirical talk about current agile practices in the real world. Both funny and satisfying as the same moment. Throwing jabs at tools and methodologies that managers accepts as holy. Satire aside, the talks highlights anti-patterns in the current agile practices. Things that we as developers perceive as non-productive, but...","categories": ["blog","software development"],
+        "tags": ["flow","Kim van Wilgen","Sander Hoogendoorn"],
+        "url": "/blog/software%20development/flow-methodology/",
+        "teaser":null}]
